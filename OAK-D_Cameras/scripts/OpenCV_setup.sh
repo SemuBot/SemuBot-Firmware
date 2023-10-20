1eeb1f3ba228058fcd137ca947aeaf250611128e
Kaur <kaur.kullamae@ut.ee>
2023-10-20 22:53:32 +0300

    Bugfixes with script and updating README

--- OAK-D_Cameras/scripts/OpenCV_setup.sh
+++ OAK-D_Cameras/scripts/OpenCV_setup.sh
@@ -4,7 +4,7 @@ sudo apt install git -y
 sudo wget -qO- https://docs.luxonis.com/install_depthai.sh | bash
 
 cd depthai
-sudo apt-get update
+sudo apt-get update -y
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt-get install python3.9 -y
 python3 depthai_demo.py
