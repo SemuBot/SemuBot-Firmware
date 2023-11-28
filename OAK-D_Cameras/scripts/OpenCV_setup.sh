eb14b9ccfe3e68227f71adf2495469964cb70290
Kaur <kaur.kullamae@ut.ee>
2023-11-28 14:35:32 +0200

    Update to OpenCV setup

--- OAK-D_Cameras/scripts/OpenCV_setup.sh
+++ OAK-D_Cameras/scripts/OpenCV_setup.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 sudo apt install git -y
+sudo yes "" | sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt -y install python3.10
+sudo apt -y install python3.10-venv
+sudo apt install python3-pip
+
 git clone https://github.com/luxonis/depthai.git "SemuBot-Electronics/OAK-D-Cameras"
 
 cd
 cd SemuBot-Electronics/OAK-D-Cameras/depthai
 sudo apt-get update -y
-sudo yes "" | sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt -y install python3.10
-sudo apt -y install python3.10-venv
+
+python3 install_requirements.py
 python3 depthai_demo.py
