837093d3d87c8f25119a5afcc66edfe6bd84227a
Kaur <kaur.kullamae@ut.ee>
2023-12-01 21:00:07 +0200

    Update setup script

--- Raspberry/scripts/setup.sh
+++ Raspberry/scripts/setup.sh
@@ -3,7 +3,8 @@
 sudo apt update
 sudo apt install python3-pip -y
 
-sudo pip3 install rpi_ws281x adafruit-circuitpython-neopixel -y #Installind necessary stuff for controlling the LED strip
+pip install Adafruit-Blinka
+sudo pip3 install rpi_ws281x adafruit-circuitpython-neopixel -y #Installing necessary stuff for controlling the LED strip
 sudo python3 -m pip install --force-reinstall adafruit-blinka -y
 
-pip install Flask -y
+pip install Flask
