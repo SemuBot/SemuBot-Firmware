8c846e6e4a92f4f44fa50d171ceb95b94d24e785
Kaur <kaur.kullamae@ut.ee>
2023-12-01 23:29:47 +0200

    Fixin setup script

--- Raspberry/scripts/setup.sh
+++ Raspberry/scripts/setup.sh
@@ -3,28 +3,11 @@
 sudo apt update
 sudo apt install python3-pip -y
 
-pip install Adafruit-Blinka
-sudo pip3 install rpi_ws281x adafruit-circuitpython-neopixel -y #Installing necessary stuff for controlling the LED strip
-sudo python3 -m pip install --force-reinstall adafruit-blinka -y
 
 cd ..
-mkdir flask
-cd flask
-python3 -m venv .venv
+mkdir environments && mkdir LedStrip && cd LedStrip
+python3 -m venv LedStrip
 
-. venv/bin/activate
+source  LedStrip/bin/activate
 pip install Flask
-cd ..
-
-sudo apt install python3.11-venv
-python -m venv env --system-site-packages
-source env/bin/activate
-
-cd ~
-pip3 install --upgrade adafruit-python-shell
-wget https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/raspi-blinka.py
-sudo -E env PATH=$PATH python3 raspi-blinka.py
-
-
-
-pip install Flask
+pip3 install adafruit-circuitpython-neonpixel
