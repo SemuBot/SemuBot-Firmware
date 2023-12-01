bc142fee62d762d4093f3200942e57a29ffe4921
Kaur <kaur.kullamae@ut.ee>
2023-12-01 23:41:21 +0200

    Fixing setup script

--- Raspberry/scripts/setup.sh
+++ Raspberry/scripts/setup.sh
@@ -4,14 +4,14 @@ sudo apt update
 sudo apt install python3-pip -y
 
 
-cd ..
-mkdir environments && cd environments && mkdir LedStrip && cd LedStrip
+cd
+mkdir environments && cd environments && mkdir LedStrip 
 python3 -m venv LedStrip
 
-source  LedStrip/bin/activate
-pip install Flask
-pip3 install adafruit-circuitpython-neonpixel
+#source  LedStrip/bin/activate # Have to do it manually
+#pip install Flask
+#pip3 install adafruit-circuitpython-neopixel
 
 cd ..
 
-python3 ~/Electronics/Raspberry/codes app.py
+#python3 ~/Electronics/Raspberry/codes app.py
