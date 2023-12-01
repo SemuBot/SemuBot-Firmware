defb1cac69f08e3b5159cd3e5de52659a6d87c6d
Kaur <kaur.kullamae@ut.ee>
2023-12-01 16:55:15 +0200

    Forgot to get the LED count number from web

--- Raspberry/codes/app.py
+++ Raspberry/codes/app.py
@@ -1,19 +1,14 @@
 from flask import Flask, render_template, request
-import time
 import board
 import neopixel
 
 app = Flask(__name__)
 
 settings = {'led_count': 0, 'rgb_values': {'red': 0, 'green': 0, 'blue': 0}}
-# D10, D12, D18 or D21
 connected_pin = board.D18
-num_pixels = 30
-# Is it RGB or GRB
+num_pixels = 30  # Default value
 ORDER = neopixel.GRB
 pixels = neopixel.NeoPixel(connected_pin, num_pixels, brightness=0.2, auto_write=False, pixel_order=ORDER)
-
-
 
 @app.route('/')
 def index():
@@ -25,45 +20,18 @@ def process_form():
         settings['rgb_values']['red'] = int(request.form['red'])
         settings['rgb_values']['green'] = int(request.form['green'])
         settings['rgb_values']['blue'] = int(request.form['blue'])
-        light_LED(settings['rgb_values']['red'], settings['rgb_values']['green'], settings['rgb_values']['blue'])
 
     if 'led_count' in request.form:
         settings['led_count'] = int(request.form['led_count'])
+        light_LED(settings['rgb_values']['red'], settings['rgb_values']['green'], settings['rgb_values']['blue'], settings['led_count'])
+
     return render_template('index.html', settings=settings)
 
-def light_LED(R,G,B):
-    pixels.fill((R,G,B))
+def light_LED(R, G, B, pixel_count):
+    global pixels
+    pixels = neopixel.NeoPixel(connected_pin, pixel_count, brightness=0.2, auto_write=False, pixel_order=ORDER)
+    pixels.fill((R, G, B))
     pixels.show()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')  # Run on all available network interfaces
+    app.run(debug=True, host='0.0.0.0')
