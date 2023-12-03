ddb7f8437028dafbaf3249c5730a83180c20e0cd
Kaur <kaur.kullamae@ut.ee>
2023-12-03 19:37:43 +0200

    Dealing with showing the picture

--- Raspberry/codes/app.py
+++ Raspberry/codes/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import board
 import neopixel
+import os
 
 app = Flask(__name__)
 
@@ -9,6 +10,9 @@ connected_pin = board.D10
 num_pixels = 30  # Default value
 ORDER = neopixel.GRB
 pixels = neopixel.NeoPixel(connected_pin, num_pixels, brightness=0.2, auto_write=False, pixel_order=ORDER)
+
+UPLOAD_FOLDER = '/home/semubot/uploads'  # Folder to store uploaded files
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def index():
@@ -25,13 +29,20 @@ def process_form():
         settings['led_count'] = int(request.form['led_count'])
         light_LED(settings['rgb_values']['red'], settings['rgb_values']['green'], settings['rgb_values']['blue'], settings['led_count'])
 
+    # Handle file upload
+    if 'image' in request.files:
+        file = request.files['image']
+        if file.filename != '':
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], 'uploaded_image.jpg')
+            file.save(file_path)
+            print("Uploaded file:", file_path)
+
     return render_template('index.html', settings=settings)
 
 def light_LED(R, G, B, pixel_count):
-    print("R:",R,"G",G,"B","Count:",pixel_count)
+    print("R:", R, "G:", G, "B:", B, "Count:", pixel_count)
     pixels.fill((R, G, B))
     pixels.show()
-    
 
-if __name__ == '__main__':
+
     app.run(debug=True, host='0.0.0.0')
