108a2b556848093f55c483128b90c3ebc5cb9ee3
Kaur <kaur.kullamae@ut.ee>
2024-02-05 08:42:27 +0200

    debug code with only arduino application needed, no platformio

--- motors/stepper_control/stepper_control.ino
+++ motors/stepper_control/stepper_control.ino
@@ -5,93 +5,63 @@ int speed = 20;
 int start = 0;
 int initialPosition = 0;
 
-int CS = 2;
-int dir = 6;
-int stepPin = 7;
+int CS = 6;
+int dir = 4;
+int stepPin = 5;
 int stepsPerRotation = 200;
-int current = 2000;
+int current = 800;
 int delayValue = 500;
 
+int speed =  0;
+int maxSpeed = 1000;
 
+TMC26XStepper tmc26XStepper = TMC26XStepper(stepsPerRotation,CS,dir,stepPin,current);
 
-TMC26XStepper tmc26XStepper = TMC26XStepper(stepsPerRotation, CS, dir, stepPin, current);
 
 void setup() {
-    Serial.begin(9600);
-
+  Serial.begin(115200);
     // Set up the stepper driver
-    tmc26XStepper.setSpreadCycleChopper(2, 24, 8, 6, 0);
-    tmc26XStepper.setRandomOffTime(0);
-    tmc26XStepper.SPI_setCoilCurrent(200);
-    tmc26XStepper.setMicrosteps(50);
-    tmc26XStepper.setStallGuardThreshold(4, 0);
-    Serial.println("Config finished, starting");
+  tmc26XStepper.setSpreadCycleChopper(2, 24, 8, 6, 0);
+  tmc26XStepper.setRandomOffTime(0);
+  tmc26XStepper.SPI_setCoilCurrent(100);
+  tmc26XStepper.setMicrosteps(128);
+  tmc26XStepper.setStallGuardThreshold(4, 0);
+  Serial.println("Config finished, starting");
 
-    initialPosition = tmc26XStepper.getMotorPosition();
-    Serial.println(initialPosition);
-    updown();
+}
+void loop() {
+  if (Serial.available() > 0) {
+    String command = Serial.readStringUntil('\n');
+    // Process the received command
+    if (command.startsWith("up")) {
+      // Extract the steps and speed numbers from the command
+      int stepsToMove = command.substring(3, command.lastIndexOf('_')).toInt();
+      int speedToSet = command.substring(command.lastIndexOf('_') + 1).toInt();
+
+      // Move the motor up
+      Serial.print("Moving up");
+      up(stepsToMove,speedToSet);
+    } else if (command.startsWith("down")) {
+      // Extract the steps number from the command
+      int stepsToMove = command.substring(5).toInt();
+      int speedToSet = command.substring(command.lastIndexOf('_') + 1).toInt();
+      // Move the motor down
+      Serial.println("Moving down");
+      down(stepsToMove,speedToSet);
+    }
+  }
 }
 
-void down() {
-    int steps = 200; 
-    tmc26XStepper.SPI_setSpeed(speed);
-    tmc26XStepper.SPI_step(-500); 
-    tmc26XStepper.spi_start();
-    start -= steps; 
-    delay(delayValue);
+void down(int steps, int speedToSet) {
+  
+    tmc26XStepper.setSpeed(speedToSet);
+    tmc26XStepper.step(10*speedToSet);
+    tmc26XStepper.move();
     Serial.println("Moving down.");
 }
 
-void up() {
-    int steps = 200;
-    tmc26XStepper.SPI_setSpeed(speed);
-    tmc26XStepper.SPI_step(500);
+void up(int steps, int speedToSet) {
+    tmc26XStepper.SPI_setSpeed(speedToSet);
+    tmc26XStepper.SPI_step(steps); 
     tmc26XStepper.spi_start();
-    start += steps;
-    delay(delayValue);
-    Serial.println("Moving up.");
-}
-void startPosition(){
-      int stepsBack = initialPosition - start;
-     Serial.print("Step back: ");
-     Serial.println(stepsBack);
-    tmc26XStepper.SPI_setSpeed(speed);
-    tmc26XStepper.SPI_step(stepsBack);
-    tmc26XStepper.spi_start();
-    start = initialPosition;
-}
-
-
-void updown(){
-  up();
-  down();
-  down();
-  up();
-}
-
-void loop() {
-    if (Serial.available() > 0) {
-        String input = Serial.readStringUntil('\n');
-      
-        if (input == "start") {
-            startPosition();
-        } else if (input == "up") {
-            up();
-        } else if (input == "down") {
-            down();
-        } else if (input == "updown"){
-          updown();
-    }else if (input.toInt() != 0) {
-            int steps = input.toInt();
-            tmc26XStepper.SPI_setSpeed(speed);
-            tmc26XStepper.SPI_step(steps);
-            tmc26XStepper.spi_start();
-            start += steps;
-            delay(500);
-            Serial.print("Stepping: ");
-            Serial.println(steps);
-        } else {
-            Serial.println("Invalid input. Please enter a valid integer or 'start'.");
-        }
-    }
 }
