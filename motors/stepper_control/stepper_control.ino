204c3bce2266607b66120edf5ee5e75a11d9a617
Kaur <kaur.kullamae@ut.ee>
2024-02-08 09:25:08 +0200

    STEP/DIR control

--- motors/stepper_control/stepper_control.ino
+++ motors/stepper_control/stepper_control.ino
@@ -5,9 +5,9 @@ int start = 0;
 int initialPosition = 0;
 
 int CS = 6;
-int dir = 4;
-int stepPin = 7;
-int stepsPerRotation = 400;
+int dir = 7;
+int stepPin = 8;
+int stepsPerRotation = 800;
 int current = 800;
 int delayValue = 500;
 int curr_step;
@@ -19,18 +19,26 @@ TMC26XStepper tmc26XStepper = TMC26XStepper(stepsPerRotation,CS,dir,stepPin,curr
 
 
 void setup() {
+
   Serial.begin(115200);
     // Set up the stepper driver
   tmc26XStepper.setSpreadCycleChopper(2, 24, 8, 6, 0);
   tmc26XStepper.setRandomOffTime(0);
-  tmc26XStepper.SPI_setCoilCurrent(100);
-  tmc26XStepper.setMicrosteps(128);
+  tmc26XStepper.SPI_setCoilCurrent(50);
+  //tmc26XStepper.setMicrosteps(248);
   tmc26XStepper.setStallGuardThreshold(4, 0);
   Serial.println("Config finished, starting");
-
 }
-/*
+// down(50,3);
+// up(50,3);
 void loop() {
+  up(50,50);
+  while (tmc26XStepper.isMoving()){
+    tmc26XStepper.getCurrentStallGuardReading();
+    tmc26XStepper.move();
+  }
+  Serial.println("Moving");
+  delay(2000);
   if (Serial.available() > 0) {
     String command = Serial.readStringUntil('\n');
     // Process the received command
@@ -42,6 +50,10 @@ void loop() {
       // Move the motor up
       Serial.print("Moving up");
       up(stepsToMove,speedToSet);
+      while (tmc26XStepper.isMoving()){
+        tmc26XStepper.getCurrentStallGuardReading();
+        tmc26XStepper.move();
+      }
     } else if (command.startsWith("down")) {
       // Extract the steps number from the command
       int stepsToMove = command.substring(5).toInt();
@@ -49,88 +61,35 @@ void loop() {
       // Move the motor down
       Serial.println("Moving down");
       down(stepsToMove,speedToSet);
+      while (tmc26XStepper.isMoving()){
+        tmc26XStepper.getCurrentStallGuardReading();
+        tmc26XStepper.move();
+      }
     }
   }
 }
-*/
 
-void loop() {
-  down(50,550);
-  //int test = tmc26XStepper.getCurrentStallGuardReading();
-  //up(50,50);
-  Serial.print("Steps left: ");
-  Serial.println(tmc26XStepper.getStepsLeft() % 100);
-  while (tmc26XStepper.getStepsLeft() % 100 == 0) {
-    //Serial.println("Stall Guard: ");
-    int test = tmc26XStepper.getCurrentStallGuardReading();
-    //Serial.println(tmc26XStepper.getCurrentStallGuardReading());
-  }
-  //tmc26XStepper.move();
-  //delay(2000);
-}
+
+
 void down(int steps, int speedToSet) {
   if (!tmc26XStepper.isMoving()) {
-    //speed += speedDirection;
-
-    if (speed > maxSpeed) {
-      speed = maxSpeed;
-    }
-
-    // Setting the speed
-    Serial.print("Setting speed to ");
-    Serial.println(speed);
-
-    tmc26XStepper.setSpeed(speed);
-
+    tmc26XStepper.setSpeed(speedToSet*10);
     // Moving the motor for a certain number of steps based on the speed
     Serial.print("Going ");
     Serial.print(10 * speed);
     Serial.println(" steps");
-    
-    tmc26XStepper.step(10 * speed);
-    tmc26XStepper.move();
-
-  } else {
-    // Output the status every 100 steps
-  
-    if (tmc26XStepper.getStepsLeft() % 100 == 0) {
-      Serial.print("Stall Guard: ");
-      Serial.println(tmc26XStepper.getCurrentStallGuardReading());
-    }
+    tmc26XStepper.step(-1000 * steps);
   }
-
-  tmc26XStepper.move();
 }
-
 void up(int steps, int speedToSet) {
   if (!tmc26XStepper.isMoving()) {
-    //speed += speedDirection;
-
-    if (speed > maxSpeed) {
-      speed = maxSpeed;
-    }
-
-    // Setting the speed
-    Serial.print("Setting speed to ");
-    Serial.println(speed);
-
-    tmc26XStepper.setSpeed(speed);
-
+    tmc26XStepper.setSpeed(speedToSet*10);
     // Moving the motor for a certain number of steps based on the speed
     Serial.print("Going ");
     Serial.print(10 * speed);
     Serial.println(" steps");
-    
-    tmc26XStepper.step(-10 * speed);
-    tmc26XStepper.move();
-
-  } else {
-    // Output the status every 100 steps
-    if (tmc26XStepper.getStepsLeft() % 100 == 0) {
-      Serial.print("Stall Guard: ");
-      Serial.println(tmc26XStepper.getCurrentStallGuardReading());
-    }
+    tmc26XStepper.step(100);
   }
-
-  tmc26XStepper.move();
 }
+
+void test()
