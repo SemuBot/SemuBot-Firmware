5a5ba545041d5d7d631f81af157eb663b9197b96
Kaur <kaur.kullamae@ut.ee>
2024-03-04 15:05:10 +0200

    Change the motor moving logic from computer

--- motors/Main/include/variables.h
+++ motors/Main/include/variables.h
@@ -5,32 +5,57 @@
 /* Stepper driver pins*/
 // Motor 1
 
-#define MOTOR1_STEP_PIN 11
-#define MOTOR1_DIR 12
-#define MOTOR1_EN_PIN 13
+#define MOTOR1_STEP_PIN 2
+#define MOTOR1_DIR 3
+#define MOTOR1_EN_PIN 4
 
 // Motor 2
 
-#define MOTOR2_DIR 757
+#define MOTOR2_DIR 5
 #define MOTOR2_STEP_PIN 6
-#define MOTOR2_EN_PIN 77
+#define MOTOR2_EN_PIN 7
 
-// Encoders
 
-#define ENCODER_CS_1 4 // Brown colour wire
-#define ENCODER_CS_2 7
+// Motor 3
+
+#define MOTOR3_DIR 8
+#define MOTOR3_STEP_PIN 9
+#define MOTOR3_EN_PIN 10
+
+
+// Motor 4
+
+#define MOTOR4_DIR 11
+#define MOTOR4_STEP_PIN 12
+#define MOTOR4_EN_PIN 13
+
+
+// Motor 5
+
+#define MOTOR5_DIR 22
+#define MOTOR5_STEP_PIN 24
+#define MOTOR5_EN_PIN 26
+
+
+
+// Encoder CS pins, brown wires
+
+#define ENCODER_CS_1 28
+#define ENCODER_CS_2 30
+#define ENCODER_CS_3 32
+#define ENCODER_CS_4 34
+#define ENCODER_CS_5 36
 
 
 /* Encoder pins */
-#define ENCODER_SCK         2 // Orange CLOCK
-#define ENCODER_SDO         3 // green DATA
+#define ENCODER_SCK 38 // Orange CLOCK
+#define ENCODER_SDO 40 // green DATA
                         // Blue is 5V
                         // Red is GND
 
-const int NUM_ENCODERS = 2;
-const int NUM_MOTORS = 2;
+const int NUM_ENCODERS = 5;
+const int NUM_MOTORS = 5;
 struct Motor {
-  String name;
   int dirPin;
   int stepPin;
   int enPin;
--- motors/Main/src/main.cpp
+++ motors/Main/src/main.cpp
@@ -5,12 +5,19 @@
 #include "variables.h"
 
 
-Motor elbowMotor = {"elbow",MOTOR1_DIR, MOTOR1_STEP_PIN, MOTOR1_EN_PIN, 100, 0, false};
-Motor shoulderMotor = {"shoulder",MOTOR2_DIR, MOTOR2_STEP_PIN, MOTOR2_EN_PIN, 100, 0, false};
+Motor elbowMotor = {MOTOR1_DIR, MOTOR1_STEP_PIN, MOTOR1_EN_PIN, 100, 0, false};
+Motor shoulderMotor = {MOTOR2_DIR, MOTOR2_STEP_PIN, MOTOR2_EN_PIN, 100, 0, false};
+Motor motor3 = {MOTOR3_DIR, MOTOR3_STEP_PIN, MOTOR3_EN_PIN, 100, 0, false};
+Motor motor4 = {MOTOR4_DIR, MOTOR4_STEP_PIN, MOTOR4_EN_PIN, 100, 0, false};
+Motor motor5 = {MOTOR5_DIR, MOTOR5_STEP_PIN, MOTOR5_EN_PIN, 100, 0, false};
+
 Encoder encoder1 = {ENCODER_CS_1, 0, elbowMotor};
 Encoder encoder2 = {ENCODER_CS_2, 0, shoulderMotor};
+Encoder encoder3 = {ENCODER_CS_3,  0, motor3};
+Encoder encoder4 = {ENCODER_CS_4, 0, motor4};
+Encoder encoder5 = {ENCODER_CS_5, 0, motor5};
+
 bool debug = true;
-int previousEncoder;
 
 Encoder encoderList[NUM_ENCODERS];
 Motor motorList[NUM_MOTORS];
@@ -20,8 +27,14 @@ void setup() {
   Serial.begin(baudRate);
   encoderList[0] = encoder1;
   encoderList[1] = encoder2;
+  encoderList[2] = encoder3;
+  encoderList[3] = encoder4;
+  encoderList[4] = encoder5;
   motorList[0] = elbowMotor;
   motorList[1] = shoulderMotor;
+  motorList[2] = motor3;
+  motorList[3] = motor4;
+  motorList[4] = motor5;
   setupMotors(motorList);
   setupEncoders(encoderList);
   encoderUpdate(encoderList);
@@ -44,60 +57,67 @@ void encoderCheck(Encoder* encoders){
   }
 }
 
-Motor get_motor_name(const String &motorName) {
+Motor* getMotorByName(const String& motorName) {
   if (motorName == "elbow") {
-    return elbowMotor;
+    return &elbowMotor;
   } else if (motorName == "shoulder") {
-    return shoulderMotor;
+    return &shoulderMotor;
+  } else if (motorName == "motor3") {
+    return &motor3;
+  } else if (motorName == "motor4") {
+    return &motor4;
+  } else if (motorName == "motor5") {
+    return &motor5;
+  } else {
+    Serial.println("Invalid motor name");
+    return nullptr;
   }
 }
-
 void loop() {
   encoderUpdate(encoderList);
-  if (!debug){
-      encoderCheck(encoderList);
-      Serial.println("Mitte siin");
-  } else{
+  if (!debug) {
+    encoderCheck(encoderList);
+  } else {
     getEncoderPosition(encoderList);
   }
-  
+
   moveMotor(elbowMotor);
   moveMotor(shoulderMotor);
- if (Serial.available() > 0) {
+  moveMotor(motor3);  
+  moveMotor(motor4);  
+  moveMotor(motor5);  
+
+  if (Serial.available() > 0) {
     String command = Serial.readStringUntil('\n');
-    
+
     if (command.startsWith("motor")) {
-      String motorName = command.substring(command.indexOf('_') + 1, command.indexOf('_', command.indexOf('_') + 1)); //command.substring(3, command.indexOf('_')); // Extract motor name
+      String motorName = command.substring(command.indexOf('_') + 1, command.indexOf('_', command.indexOf('_') + 1));
       String action = command.substring(command.indexOf('_', command.indexOf('_') + 1) + 1, command.indexOf('_', command.indexOf('_', command.indexOf('_') + 1) + 1));
-      int stepsToMove = command.substring(command.lastIndexOf('_') + 1).toInt(); // Extract steps
+      int stepsToMove = command.substring(command.lastIndexOf('_') + 1).toInt();
 
       Serial.println("Motor: " + motorName + ", Action: " + action + ", Steps: " + String(stepsToMove));
+      
       if (action == "up") {
-        if (motorName == "elbow"){
-          set_Steps(elbowMotor,stepsToMove);
-          startMotor(elbowMotor);
-        } else{
-          set_Steps(shoulderMotor,stepsToMove);
-          startMotor(shoulderMotor);
+        Motor* motor = getMotorByName(motorName);
+        if (motor != nullptr) {
+          set_Steps(*motor, stepsToMove);
+          startMotor(*motor);
         }
       } else if (action == "down") {
-        if (motorName == "shoulder") {
-          set_Steps(shoulderMotor, -stepsToMove);
-          startMotor(shoulderMotor);
-        } else {
-          set_Steps(elbowMotor, -stepsToMove);
-          startMotor(elbowMotor);
+        Motor* motor = getMotorByName(motorName);
+        if (motor != nullptr) {
+          set_Steps(*motor, -stepsToMove);
+          startMotor(*motor);
         }
-
       } else if (action == "stop") {
         Serial.println("STOPPING Motors");
         stopMotor(elbowMotor);
         stopMotor(shoulderMotor);
+        stopMotor(motor3);
+        stopMotor(motor4);
+        stopMotor(motor5);
       }
     }
   }
 }
-
-
-
 
