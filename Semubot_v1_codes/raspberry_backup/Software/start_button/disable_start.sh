b3488da25ad278546c0560f393a5599a63dd94d2
Semubot <email>
2026-01-18 10:18:26 +0000

    Save before subtree

--- Semubot_v1_codes/raspberry_backup/Software/start_button/disable_start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-crontab -l > mycron_backup
-
-sed -i '24 s/^/#/' mycron_backup
-
-crontab mycron_backup
-echo "Auto start has been disabled."
-
--- Semubot_v1_codes/raspberry_backup/Software/start_button/run_in_screen.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-screen -dmS button python3 /home/semubot/Software/start_button/button.py
